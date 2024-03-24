fc0c7cc914a3a2cfbe3451485a19225750db506b
nyxxy <gladilinnikita@yandex.ru>
2024-03-24 03:15:29 +0300

    Создать реализацию двусвязного списка для того, чтобы убрать повторы и ограничение по размеру для истории просмотров, покрыть новую реализацию тестами.

---
+++ src/TaskManager/Node.java
@@ -0,0 +1,15 @@
+package TaskManager;
+
+import Tasks.Task;
+
+public class Node {
+	public Task data;
+	public Node next;
+	public Node prev;
+
+	public Node(Node prev, Task data, Node next) {
+		this.data = data;
+		this.next = next;
+		this.prev = prev;
+	}
+}
