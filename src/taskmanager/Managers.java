890f5e439c896e774b43ed30ca6a61815160e0c0
nyxxy <gladilinnikita@yandex.ru>
2024-03-24 04:02:15 +0300

    Исправления по стилю

--- src/taskmanager/Managers.java
+++ src/taskmanager/Managers.java
@@ -2,14 +2,16 @@ package taskmanager;
 
 public final class Managers {
 
-	private Managers(){}
+	private Managers() {
+
+	}
 
 	public static TaskManager getDefault() {
 		return new InMemoryTaskManager();
 
-
 	}
-	public static HistoryManager getDefaultHistory(){
+
+	public static HistoryManager getDefaultHistory() {
 		return new InMemoryHistoryManager();
 
 	}
